304aa344ecfa75999f696c85ca32e1da63770d1f
Tom Childress <thomas.childress02@gmail.com>
2024-05-12 20:04:31 -0400

    completed chapter 15

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,8 @@ import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 
 const FormSchema = z.object({
   id: z.string(),
@@ -75,8 +77,8 @@ export const updateInvoice = async (
 
   if (!validatedFields.success) {
     return {
-        errors: validatedFields.error.flatten().fieldErrors,
-        message: 'Missing fields. Failed to update invoice.',
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing fields. Failed to update invoice.',
     };
   }
 
@@ -107,3 +109,22 @@ export const deleteInvoice = async (
   }
   revalidatePath('/dashboard/invoices');
 };
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
+}
